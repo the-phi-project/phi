670cbbdd7621c8acc1bcb3ee6c7e4c3487d71087
zeviberlin <zeviberlin@gmail.com>
2026-03-01 22:39:52 -0500

    full validation model for ed25519

--- include/encryption/asymmetric/ed25519.hpp
+++ include/encryption/asymmetric/ed25519.hpp
@@ -24,8 +24,6 @@
 
 namespace phi::encryption {
 
-
-
 /*
 Generate a public/private key
  pair using Crypto++ ed25519 implementation
@@ -68,10 +66,27 @@ static std::string ed25519ToStr(const KeyType& key) {
 template <typename KeyType>
 static KeyType ed25519FromStr(const std::string& str) {
   KeyType key;
-  CryptoPP::StringSource keySource(str, true);
-  key.Load(keySource);
+  CryptoPP::StringSource key_source(str, true);
+  key.Load(key_source);
 
   return key;
+}
+
+//================={ Header Item Separator }=================\\ 
+
+/*
+Validate a CryptoPP::ed25519 key fully
+*/
+template <typename KeyType>
+static bool ed25519ValidateKey(const std::string& str) {
+  CryptoPP::AutoSeededRandomPool rng;
+  try {
+    KeyType key = ed25519FromStr<KeyType>(str);
+    // level 2 - make sure this object will function correctly, and do reasonable security checks
+    return key.Validate(rng, 2);
+  } catch (const CryptoPP::BERDecodeErr& exc) {
+    return false;
+  }
 }
 
 }  // namespace phi::encryption
